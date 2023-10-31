85a6ecc42d4b21088e135a56265cece084113a6a
Derek <dglassick@perrinwear.com>
2023-10-31 11:13:31 -0400

    started creating player table

--- app/tables/page.tsx
+++ app/tables/page.tsx
@@ -1,9 +1,12 @@
+"use client"
+
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import TableOne from "@/components/Tables/TableOne";
 import TableThree from "@/components/Tables/TableThree";
 import TableTwo from "@/components/Tables/TableTwo";
 
 import { Metadata } from "next";
+import { PLAYER_DATA } from "../table/data";
 export const metadata: Metadata = {
   title: "Tables Page | Next.js E-commerce Dashboard Template",
   description: "This is Tables page for TailAdmin Next.js",
@@ -15,10 +18,10 @@ const TablesPage = () => {
     <>
       <Breadcrumb pageName="Tables" />
 
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-10 w-full max-h-10/12">
         <TableOne />
-        <TableTwo />
-        <TableThree />
+        {/* <TableTwo />
+        <TableThree /> */}
       </div>
     </>
   );

bab7ef8c6e61c225d4ea5977f20057e49c2bbe71
Derek <dglassick@perrinwear.com>
2023-11-02 13:10:02 -0400

    Added ability to drag on field

--- app/matchinput/page.tsx
+++ app/matchinput/page.tsx
@@ -4,8 +4,10 @@ import { Box } from '@chakra-ui/react';
 import React, { useState, useRef } from 'react'
 
 const MatchInput = () => {
-    const fieldRef = useRef(null)
+    const canvasRef = useRef(null);
     const [coordinates, setCoordinates] = useState([]);
+    const [isMouseDown, setIsMouseDown] = useState(false);
+    const [startPoint, setStartPoint] = useState(null);
 
     const handleClick = (e) => {
         console.log(e);
@@ -17,9 +19,36 @@ const MatchInput = () => {
         setCoordinates([...coordinates, { x, y }]);
     };
 
+    const handleMouseDown = (e) => {
+        setIsMouseDown(true);
+    
+        const rect = canvasRef.current.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+    
+        setStartPoint({ x, y });
+      };
+    
+      const handleMouseUp = () => {
+        setIsMouseDown(false);
+        setStartPoint(null);
+      };
+    
+      const handleMouseMove = (e) => {
+        if (isMouseDown) {
+          const rect = canvasRef.current.getBoundingClientRect();
+          const x = e.clientX - rect.left;
+          const y = e.clientY - rect.top;
+    
+          setCoordinates([...coordinates, { x, y }]);
+        }
+      };
+
     return (
         <Box className="pitch">
-            <Box className="overlay" onClick={handleClick}></Box>
+            <Box className="overlay" onClick={handleClick} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}>
+            <canvas ref={canvasRef} className="canvas"></canvas>
+            </Box>
             <Box className="field left">
                 <Box className="penalty-area">
                 </Box>
@@ -32,6 +61,9 @@ const MatchInput = () => {
             {coordinates.map((coord, index) => (
                 <Box key={index} position={'absolute'} width={'10px'} height={'10px'} backgroundColor={'#ff0000'} borderRadius={'50%'} style={{ left: coord.x, top: coord.y }} />
             ))}
+            {startPoint && (
+                <div className="pass-dot" style={{ left: startPoint.x, top: startPoint.y }}></div>
+            )}
         </Box>
 
     );

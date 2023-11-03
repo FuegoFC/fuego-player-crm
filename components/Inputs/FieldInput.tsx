9de44ec7a3bb0f74c9a11b6e5ce7bdee15276410
Derek Glassick <derek.glassick@gmail.com>
2023-11-02 21:21:50 -0400

    Updates to the forms

--- components/Inputs/FieldInput.tsx
+++ components/Inputs/FieldInput.tsx
@@ -2,6 +2,7 @@
 
 import { Box } from '@chakra-ui/react';
 import React, { useState, useRef } from 'react'
+import { PiSoccerBallFill } from 'react-icons/pi'
 
 type Coordinate = {
     x: number,
@@ -22,12 +23,11 @@ type FieldInputProps = {
 }
 
 const FieldInput = (props: FieldInputProps) => {
-    const { actionType = 'pass' } = props;
-    const canvasRef = useRef(null); 
+    const { actionType } = props;
+    const canvasRef = useRef(null);
     const [passPath, setPassPath] = useState<PassPath[]>([]);
     const [shotCoordinates, setShotCoordinates] = useState<Coordinate[]>([]);
     const [passCoordinates, setPassCoordinates] = useState<PassCoordinates[]>([]);
-    const [isMouseDown, setIsMouseDown] = useState(false);
     const [startPoint, setStartPoint] = useState<Coordinate | null>(null);
 
     const handleClick = (e) => {
@@ -41,35 +41,33 @@ const FieldInput = (props: FieldInputProps) => {
     };
 
     const handleMouseDown = (e) => {
-        setIsMouseDown(true);
         const rect = e.target.getBoundingClientRect();
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
-    
-        setStartPoint({ x, y });
-        setPassCoordinates([...passCoordinates, {x, y, passer: true}])
-      };
-    
-      const handleMouseUp = (e) => {
-        if (startPoint) {
-          const rect = e.target.getBoundingClientRect();
-          const x = e.clientX - rect.left;
-          const y = e.clientY - rect.top;
-    
-          const pass = { start: startPoint, end: { x, y } };
-          setPassPath([...passPath, pass]);
-          setPassCoordinates([...passCoordinates, {x, y, passer: false}])
-          setStartPoint(null);
-        }
-        setIsMouseDown(false);
-      };
 
-      console.log(shotCoordinates)
+        setStartPoint({ x, y });
+        setPassCoordinates([...passCoordinates, { x, y, passer: true }])
+    };
+
+    const handleMouseUp = (e) => {
+        if (startPoint) {
+            const rect = e.target.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            const pass = { start: startPoint, end: { x, y } };
+            setPassPath([...passPath, pass]);
+            setPassCoordinates([...passCoordinates, { x, y, passer: false }])
+            setStartPoint(null);
+        }
+    };
+
+    console.log(shotCoordinates)
 
     return (
         <Box className="pitch">
-            <Box className="overlay" onClick={(e) => actionType === 'shot' ? handleClick(e) : null} onMouseDown={(e) => actionType === 'pass' ? handleMouseDown(e) : null } onMouseUp={(e) => actionType === 'pass' ? handleMouseUp(e) : null }>
-            <canvas ref={canvasRef} className="canvas"></canvas>
+            <Box className="overlay" onClick={(e) => actionType === 'shot' ? handleClick(e) : null} onMouseDown={(e) => actionType === 'pass' ? handleMouseDown(e) : null} onMouseUp={(e) => actionType === 'pass' ? handleMouseUp(e) : null}>
+                <canvas ref={canvasRef} className="canvas"></canvas>
             </Box>
             <Box className="field left">
                 <Box className="penalty-area">
@@ -81,22 +79,22 @@ const FieldInput = (props: FieldInputProps) => {
             </Box>
             <Box className="center-circle"></Box>
             {actionType === 'shot' && shotCoordinates.map((coord, index) => (
-                <Box key={index} position={'absolute'} width={'10px'} height={'10px'} backgroundColor={'#ff0000'} borderRadius={'50%'} zIndex={1} style={{ left: coord.x, top: coord.y }} />
+                <Box key={index} position={'absolute'} width={'10px'} height={'10px'} borderRadius={'50%'} zIndex={1} style={{ left: coord.x, top: coord.y }}><PiSoccerBallFill style={{ color: 'white' }} /></Box>
             ))}
             {actionType === 'pass' && passCoordinates.map((coord, index) => (
-                <Box key={index} position={'absolute'} width={'10px'} height={'10px'} backgroundColor={coord.passer ? 'blue' : 'yellow'} zIndex={1} borderRadius={'50%'} style={{ left: coord.x, top: coord.y }} />
+                <Box key={index} position={'absolute'} width={'10px'} height={'10px'} zIndex={1} borderRadius={'50%'} style={{ left: coord.x, top: coord.y }}><PiSoccerBallFill color={coord.passer ? 'blue' : 'yellow'} /></Box>
             ))}
             {actionType === 'pass' && <svg className="canvas">
                 {passPath.map((pass, index) => (
-                <path
-                    key={index}
-                    // Adding 5 helps center the path
-                    d={`M ${pass.start.x + 5} ${pass.start.y + 5} L ${pass.end.x + 5} ${pass.end.y + 5}`}
-                    stroke="blue"
-                    strokeWidth="2"
-                    strokeDasharray={'5,5'}
-                    fill="transparent"
-                />
+                    <path
+                        key={index}
+                        // Adding 5 helps center the path
+                        d={`M ${pass.start.x + 7} ${pass.start.y + 7} L ${pass.end.x + 7} ${pass.end.y + 7}`}
+                        stroke="red"
+                        strokeWidth="2"
+                        strokeDasharray={'5,5'}
+                        fill="transparent"
+                    />
                 ))}
             </svg>}
         </Box>
